48c75c0b211e9f4cdd8dd6e74d1665ade307137c
bygithub <becasewithoutu@gmail.com>
2015-02-13 09:59:56 +0800

    new

---
+++ App/Admin/Controller/TestController.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Admin\Controller;
+use Admin\Controller\CommonController;
+class TestController extends CommonController{
+    public function read(){
+        echo '欢迎来到普通会员中心';
+    }
+    public function write(){
+        echo "欢迎来到白银会员中心";
+    }
+    public function change(){
+        echo "欢迎来到黄金会员中心";
+    }
+    public function delete(){
+        echo "欢迎来到钻石会员中心";
+    }
+}
+
+
+?>
